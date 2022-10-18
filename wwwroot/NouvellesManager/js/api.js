0e4b4299e6cff481cde59b175bc5b13bac43421b
Jeremy Labelle <jgoodgoal@hotmail.ca>
2022-10-18 14:31:02 -0400

    Bonne base Url

--- wwwroot/NouvellesManager/js/api.js
+++ wwwroot/NouvellesManager/js/api.js
@@ -1,4 +1,4 @@
-const apiBaseURL = "http://localhost:5000/api/nouvelles";
+const apiBaseURL = "https://labo-3---mathieu-bussiere-et-jeremy-labelle.glitch.me/NouvellesManager/index.html";
 
 function HEAD(successCallBack, errorCallBack) {
     $.ajax({
